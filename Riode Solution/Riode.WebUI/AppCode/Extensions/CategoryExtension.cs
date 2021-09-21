e7e9882e2a4bb6cc25bce69749c66a8358f78aa9
ilhambagirov <ilhambb@code.edu.az>
2021-09-20 17:50:36 -0700

    Category/Admin Extension (90%)

--- Riode Solution/Riode.WebUI/AppCode/Extensions/CategoryExtension.cs
+++ Riode Solution/Riode.WebUI/AppCode/Extensions/CategoryExtension.cs
@@ -41,5 +41,47 @@ namespace Riode.WebUI.AppCode.Extensions
             }
         }
 
+
+        public static string GetCategoriesRawAdmin(this IEnumerable<Category> categories)
+        {
+            if (categories == null && !categories.Any())
+            {
+                return "";
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in categories)
+            {
+                sb.Append($"<tr class='treegrid-{item.Id}'>");
+
+                GetChildrenRaw(item);
+            }
+            sb.Append("</tr>");
+
+            return sb.ToString();
+
+            void GetChildrenRaw(Category category)
+            {
+                sb.Append("<td>");
+                sb.Append($"{category.Name}");
+                sb.Append("</td>");
+                sb.Append("<td class='d-flex justify-content-end'>");
+                sb.Append($"<a asp-controller='Categories' asp-action='details' asp-route-id='{category.Id}' class='btn btn-success mr-1'><i class='fas fa-info'></i></a>" +
+                                                    $"<a asp-controller = 'Categories' asp-action = 'edit' asp-route-id = '{category.Id}' class='btn btn-warning mr-1'><i class='fas fa-user-edit'></i></a>" +
+                                                   $"<a asp-controller='Categories' asp-action='delete' asp-route-id='{category.Id}' class='btn btn-danger mr-1'><i class='fas fa-trash'></i></a>");
+                sb.Append("</td>");
+                if (category.Children != null && category.Children.Any())
+                {
+
+                    foreach (var item in category.Children)
+                    {
+                        sb.Append($"<tr class='treegrid-{item.Id} treegrid-parent-{item.ParentId}'>");
+                        GetChildrenRaw(item);
+                    }
+                    sb.Append("</tr>");
+                }
+
+
+            }
+        }
     }
 }
