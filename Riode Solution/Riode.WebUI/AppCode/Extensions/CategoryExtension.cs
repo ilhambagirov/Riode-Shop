d3a4db2af212a9f63e4e14bac0d043176fbd1cad
ilhambagirov <ilhambb@code.edu.az>
2021-09-23 04:17:16 -0700

    Select2

--- Riode Solution/Riode.WebUI/AppCode/Extensions/CategoryExtension.cs
+++ Riode Solution/Riode.WebUI/AppCode/Extensions/CategoryExtension.cs
@@ -43,46 +43,64 @@ namespace Riode.WebUI.AppCode.Extensions
             }
         }
 
+        //one way to do it
+        //public static string GetCategoriesRawAdmin(this IEnumerable<Category> categories, IUrlHelper url)
+        //{
+        //    if (categories == null && !categories.Any())
+        //    {
+        //        return "";
+        //    }
+        //    StringBuilder sb = new StringBuilder();
+        //    foreach (var item in categories)
+        //    {
+        //        sb.Append($"<tr class='treegrid-{item.Id}'>");
 
-        public static string GetCategoriesRawAdmin(this IEnumerable<Category> categories, IUrlHelper url)
+        //        GetChildrenRaw(item);
+        //    }
+        //    sb.Append("</tr>");
+
+        //    return sb.ToString();
+
+        //    void GetChildrenRaw(Category category)
+        //    {
+        //        sb.Append("<td>");
+        //        sb.Append($"{category.Name}");
+        //        sb.Append("</td>");
+        //        sb.Append("<td class='d-flex justify-content-end'>");
+        //        sb.Append($"<a href={url.Action("details","categories",values:new { id= category.Id })} class='btn btn-success mr-1'><i class='fas fa-info'></i></a>" +
+        //                                            $"<a asp-controller = 'Categories' asp-action = 'edit' asp-route-id = '{category.Id}' class='btn btn-warning mr-1'><i class='fas fa-user-edit'></i></a>" +
+        //                                           $"<a asp-controller='Categories' asp-action='delete' asp-route-id='{category.Id}' class='btn btn-danger mr-1'><i class='fas fa-trash'></i></a>");
+        //        sb.Append("</td>");
+        //        if (category.Children != null && category.Children.Any())
+        //        {
+
+        //            foreach (var item in category.Children)
+        //            {
+        //                sb.Append($"<tr class='treegrid-{item.Id} treegrid-parent-{item.ParentId}'>");
+        //                GetChildrenRaw(item);
+        //            }
+        //            sb.Append("</tr>");
+        //        }
+
+
+        //    }
+        //}
+
+
+
+        //second way to do it
+        static public IEnumerable<Category> GetChildren(this Category category)
         {
-            if (categories == null && !categories.Any())
+            if (category.ParentId != null)
             {
-                return "";
+                yield return category;
             }
-            StringBuilder sb = new StringBuilder();
-            foreach (var item in categories)
+            if (category.Children != null)
             {
-                sb.Append($"<tr class='treegrid-{item.Id}'>");
-
-                GetChildrenRaw(item);
-            }
-            sb.Append("</tr>");
-
-            return sb.ToString();
-
-            void GetChildrenRaw(Category category)
-            {
-                sb.Append("<td>");
-                sb.Append($"{category.Name}");
-                sb.Append("</td>");
-                sb.Append("<td class='d-flex justify-content-end'>");
-                sb.Append($"<a href={url.Action("details","categories",values:new { id= category.Id })} class='btn btn-success mr-1'><i class='fas fa-info'></i></a>" +
-                                                    $"<a asp-controller = 'Categories' asp-action = 'edit' asp-route-id = '{category.Id}' class='btn btn-warning mr-1'><i class='fas fa-user-edit'></i></a>" +
-                                                   $"<a asp-controller='Categories' asp-action='delete' asp-route-id='{category.Id}' class='btn btn-danger mr-1'><i class='fas fa-trash'></i></a>");
-                sb.Append("</td>");
-                if (category.Children != null && category.Children.Any())
+                foreach (var item in category.Children.SelectMany(c => c.GetChildren()))
                 {
-
-                    foreach (var item in category.Children)
-                    {
-                        sb.Append($"<tr class='treegrid-{item.Id} treegrid-parent-{item.ParentId}'>");
-                        GetChildrenRaw(item);
-                    }
-                    sb.Append("</tr>");
+                    yield return item;
                 }
-
-
             }
         }
     }
