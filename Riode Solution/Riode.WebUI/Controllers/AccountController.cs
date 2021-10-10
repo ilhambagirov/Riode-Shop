7c59a09ea808acbe50852f16fbe686ea77ff110b
ilhambagirov <ilhambb@code.edu.az>
2021-10-10 00:58:48 -0700

    Check

--- Riode Solution/Riode.WebUI/Controllers/AccountController.cs
+++ Riode Solution/Riode.WebUI/Controllers/AccountController.cs
@@ -39,6 +39,12 @@ namespace Riode.WebUI.Controllers
             return View();
         }
 
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+        public IActionResult PopupLogin()
+=======
+=======
+>>>>>>> origin/feature-membership
         [HttpPost]
         public async Task<IActionResult> Login(LoginFormModel model)
         {
@@ -92,6 +98,14 @@ namespace Riode.WebUI.Controllers
             return View(model);
         }
 
+<<<<<<< HEAD
+        public async Task<IActionResult>Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("login") ;
+        }
+=======
+>>>>>>> origin/feature-membership
         public IActionResult Register()
         {
             return View();
